00c8b8737cd39d726f8fa44870ff65b05545e4a3
ozguney <ozgungg@gmail.com>
2021-11-16 16:28:59 +0300

    velocity eklendi

--- amtrakdata.py
+++ amtrakdata.py
@@ -57,3 +57,80 @@ fig.update_layout(
 
 #   Writing Figure to HTML
 fig.write_html("02_BoxView.html")
+
+# haversine formula: takes degrees
+def dist_between(lat1, lon1, lat2, lon2):
+    R = 6371e3 #metres
+    phi1 = lat1 * np.pi/180
+    phi2 = lat2 * np.pi/180
+    deltaPhi = (lat2-lat1) * np.pi/180
+    deltaLambda = (lon2-lon1) * np.pi/180
+    
+    a = np.sin(deltaPhi/2)**2 + np.cos(phi1) * np.cos(phi2) * (np.sin(deltaLambda/2)**2)
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+    return R * c # meters
+
+def test_dist_between():
+    lat1 = gpxDF['lat'].iloc[0]
+    lon1 = gpxDF['lon'].iloc[0]
+    lat2 = gpxDF['lat'].iloc[-1]
+    lon2 = gpxDF['lon'].iloc[-1]
+    d = dist_between(lat1, lon1, lat2, lon2)
+    print("dist (meters): ", d)
+    dMiles = d/1609.34
+    print("dist (miles): ", dMiles)
+
+# velocity calculation
+gpxDF["deltaDistMeters"] = dist_between(gpxDF['lat'].shift(),
+                                        gpxDF['lon'].shift(),
+                                        gpxDF.loc[1:, 'lat'],
+                                        gpxDF.loc[1:, 'lon'])
+gpxDF["deltaTimeSeconds"] = (gpxDF.loc[1:, 'time'] - gpxDF['time'].shift()).apply(lambda row: row.total_seconds())
+gpxDF["velocityMetersPerSecond"] = gpxDF["deltaDistMeters"] / gpxDF["deltaTimeSeconds"]
+gpxDF["velocityMilesPerHour"] = gpxDF["velocityMetersPerSecond"] * (3600.0 / 1609.34)
+
+gpxDFSpeedFiltered = gpxDF.loc[(gpxDF["velocityMilesPerHour"] >= 0) & (gpxDF["velocityMilesPerHour"] < 150)]
+gpxDFSpeedFiltered.describe()
+
+# Plot velocity
+
+fig = px.line_3d(gpxDFSpeedFiltered, x="lon", y="lat", z="velocityMetersPerSecond")
+fig.add_trace(go.Scatter3d(
+    x = [gpxDFSpeedFiltered['lon'].iloc[0]], 
+    y = [gpxDFSpeedFiltered['lat'].iloc[0]],
+    z = [gpxDFSpeedFiltered['velocityMetersPerSecond'].iloc[0]],
+    name = 'Starting Point'
+))
+
+fig.add_trace(go.Scatter3d(
+    x = [gpxDFSpeedFiltered['lon'].iloc[-1]], 
+    y = [gpxDFSpeedFiltered['lat'].iloc[-1]],
+    z = [gpxDFSpeedFiltered['velocityMetersPerSecond'].iloc[-1]],
+    name = 'Destination Point'
+))
+
+velocityColors = []
+for i in range(0, len(gpxDFSpeedFiltered)):
+    v = gpxDFSpeedFiltered['velocityMetersPerSecond'].iloc[i]
+    if v <= 1:
+        color = 'red'
+    elif 1 < v <= 3:
+        color = 'yellow'
+    elif 3 < v <= 50:
+        color = 'green'
+    else:
+        color = 'black'
+    velocityColors.append(color)
+
+fig.add_trace(go.Scatter3d(
+    x = gpxDFSpeedFiltered['lon'], 
+    y = gpxDFSpeedFiltered['lat'],
+    z = [0 for n in range(len(gpxDFSpeedFiltered))],
+    name = 'Route',
+    line=dict(color=velocityColors, width=0)
+))
+    
+fig.update_layout(
+    title = "Velocity vs lat,lon"
+)
+fig.write_html("03_Velocity.html")
