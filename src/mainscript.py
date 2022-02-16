05930bcc307d5f04f03a6bce85c7ec24a8a22144
Ozgun <ozgungg@gmail.com>
2022-02-16 21:29:42 +0300

    Update mainscript.py

--- src/mainscript.py
+++ src/mainscript.py
@@ -83,10 +83,10 @@ fig_VelocityHeatMap.show()
 
 #### WRITING ####
 
-fig_MapBox.write_html("fig_MapBox.html")
-fig_Scatter3d.write_html("fig_Scatter3d.html")
-fig_Scatter3dVelocity.write_html("fig_Scatter3dVelocity.html")
-fig_VelocityTimeGraph.write_html("fig_VelocityTimeGraph.html")
-fig_ElevationTimeGraph.write_html("fig_ElevationTimeGraph.html")
-fig_VelocityElevationCombined.write_html("fig_VelocityElevationCombined.html")
-fig_VelocityHeatMap.write_html("fig_VelocityHeatMap.html")
+# fig_MapBox.write_html("fig_MapBox.html")
+# fig_Scatter3d.write_html("fig_Scatter3d.html")
+# fig_Scatter3dVelocity.write_html("fig_Scatter3dVelocity.html")
+# fig_VelocityTimeGraph.write_html("fig_VelocityTimeGraph.html")
+# fig_ElevationTimeGraph.write_html("fig_ElevationTimeGraph.html")
+# fig_VelocityElevationCombined.write_html("fig_VelocityElevationCombined.html")
+# fig_VelocityHeatMap.write_html("fig_VelocityHeatMap.html")
