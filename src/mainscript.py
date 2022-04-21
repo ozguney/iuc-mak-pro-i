82e2339899a1360e1da531220f573345e3e29aff
Ozgun <ozgungg@gmail.com>
2022-04-21 15:52:25 +0300

    using geo's haversine distance instead of dist_between func

--- src/mainscript.py
+++ src/mainscript.py
@@ -1,5 +1,5 @@
-import pandas as pd # pandas 1.3.5
-import numpy as np # numpy 1.22.2
+import pandas as pd  # pandas 1.3.5
+import numpy as np  # numpy 1.22.2
 import gpxpy
 import gpxpy.gpx
 import matplotlib.pyplot as plt
@@ -14,53 +14,62 @@ from gpxpy.gpx import *
 from scipy.signal import argrelextrema
 import matplotlib.pyplot as plt
 
-def dist_between(lat1, lon1, lat2, lon2):
-    R = 6371e3 #metres
-    phi1 = lat1 * np.pi/180
-    phi2 = lat2 * np.pi/180
-    deltaPhi = (lat2-lat1) * np.pi/180
-    deltaLambda = (lon2-lon1) * np.pi/180
-    
-    a = np.sin(deltaPhi/2)**2 + np.cos(phi1) * np.cos(phi2) * (np.sin(deltaLambda/2)**2)
-    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
-    return R * c # meters
 def ElevationAngle(ele1, ele2, distance):
     return np.arctan((ele2-ele1)/distance)
+
+
 def DataFrameCalculations(df):
-    df.drop_duplicates(subset=['time'], keep=False) # Deleting duplicated times
-    df["deltaDistMeters"] = dist_between(df['lat'].shift(),
-                                        df['lon'].shift(),
-                                        df.loc[1:, 'lat'],
-                                        df.loc[1:, 'lon'])
+    # Deleting duplicated times
+    df.drop_duplicates(subset=['time'], keep=False)
+    df["deltaDistMeters"] = haversine_distance(df['lat'].shift(),
+                                         df['lon'].shift(),
+                                         df.loc[1:, 'lat'],
+                                         df.loc[1:, 'lon'])
     df["deltaElevationMeters"] = df["ele"].diff()
     df["elevationAngle"] = ElevationAngle(df['ele'].shift(),
                                           df.loc[1:, 'ele'],
                                           df["deltaDistMeters"])
 
-    df["deltaTimeSeconds"] = (df.loc[1:, 'time'] - df['time'].shift()).apply(lambda row: row.total_seconds())
+    df["deltaTimeSeconds"] = (
+        df.loc[1:, 'time'] - df['time'].shift()).apply(lambda row: row.total_seconds())
     df["cumTime"] = df["deltaTimeSeconds"].cumsum()
-    df = df[df.deltaDistMeters != 0] # Deleting 0 meter rows
-    df = df.reset_index(drop=True) # Resetting index values due to deleting 0 second rows
-    df["velocityKmPerHour"] = df["deltaDistMeters"] / df["deltaTimeSeconds"] * (3600.0 / 1000)
+    df = df[df.deltaDistMeters != 0]  # Deleting 0 meter rows
+    # Resetting index values due to deleting 0 second rows
+    df = df.reset_index(drop=True)
+    df["velocityKmPerHour"] = df["deltaDistMeters"] / \
+        df["deltaTimeSeconds"] * (3600.0 / 1000)
     df = df[df.velocityKmPerHour < 50]
     df = df.reset_index(drop=True)
     return df
+
+
 def DataFrameSmoothing(df):
-    df["velocityKmPerHour_ma100"] = df["velocityKmPerHour"].rolling(window=100).mean()
-    df["velocityKmPerHour_ma20"] = df["velocityKmPerHour"].rolling(window=20).mean()
+    df["velocityKmPerHour_ma100"] = df["velocityKmPerHour"].rolling(
+        window=100).mean()
+    df["velocityKmPerHour_ma20"] = df["velocityKmPerHour"].rolling(
+        window=20).mean()
     return df
+
+
 def GroupSlopes(df):
-    n = 50 # number of points to be checked before and after
-    df['min'] = df.iloc[argrelextrema(df.ele.values, np.less_equal,order=n)[0]]['ele']
-    df['max'] = df.iloc[argrelextrema(df.ele.values, np.greater_equal,order=n)[0]]['ele']
-    
-    df = df[df['min'].shift() != df['min']] # Dropping duplicated min max values
+    n = 50  # number of points to be checked before and after
+    df['min'] = df.iloc[argrelextrema(
+        df.ele.values, np.less_equal, order=n)[0]]['ele']
+    df['max'] = df.iloc[argrelextrema(
+        df.ele.values, np.greater_equal, order=n)[0]]['ele']
+
+    # Dropping duplicated min max values
+    df = df[df['min'].shift() != df['min']]
     df = df[df['max'].shift() != df['max']]
     return df
+
+
 def CumulativeElevationDistance(df):
     df["cumElevation"] = df["deltaElevationMeters"].cumsum()
     df["cumDistance"] = df["deltaDistMeters"].cumsum()
     return df
+
+
 def ElevationGradients(df):
     gradients = [np.nan]
     for ind, row in df.iterrows():
@@ -74,35 +83,43 @@ def ElevationGradients(df):
         else:
             gradients.append(np.round(grade, 1))
     df["elevationGradients"] = gradients
-    df = df.reset_index(drop=True) # Resetting index values due to deleting nan rows
+    # Resetting index values due to deleting nan rows
+    df = df.reset_index(drop=True)
     return df
+
+
 def GradientRangeTagging(df):
     bins = pd.IntervalIndex.from_tuples([
-    (-30, -10),
-    (-10, -5), 
-    (-5, -3), 
-    (-3, -1), 
-    (-1, 1),
-    (1, 3), 
-    (3, 5), 
-    (5, 10), 
-    (10, 30)],
-    closed='right')
+        (-30, -10),
+        (-10, -5),
+        (-5, -3),
+        (-3, -1),
+        (-1, 1),
+        (1, 3),
+        (3, 5),
+        (5, 10),
+        (10, 30)],
+        closed='right')
     df["gradientRange"] = pd.cut(df["elevationGradients"], bins=bins)
     return df
+
+
 def GradientRangeTagDetails(df):
     gradient_details = []
 
     for gr_range in df['gradientRange'].unique():
         # Keep that subset only
         subset = df[df['gradientRange'] == gr_range]
-        
+
         # Statistics
         total_distance = subset['deltaDistMeters'].sum()
-        pct_of_total_ride = (subset['deltaDistMeters'].sum() / df['deltaDistMeters'].sum()) * 100
-        elevation_gain = subset[subset['deltaElevationMeters'] > 0]['deltaElevationMeters'].sum()
-        elevation_lost = subset[subset['deltaElevationMeters'] < 0]['deltaElevationMeters'].sum()
-        
+        pct_of_total_ride = (
+            subset['deltaDistMeters'].sum() / df['deltaDistMeters'].sum()) * 100
+        elevation_gain = subset[subset['deltaElevationMeters']
+                                > 0]['deltaElevationMeters'].sum()
+        elevation_lost = subset[subset['deltaElevationMeters']
+                                < 0]['deltaElevationMeters'].sum()
+
         # Save results
         gradient_details.append({
             'gradient_range': gr_range,
@@ -111,8 +128,11 @@ def GradientRangeTagDetails(df):
             'elevation_gain': np.round(elevation_gain, 2),
             'elevation_lost': np.round(np.abs(elevation_lost), 2)
         })
-    gradient_details_df = pd.DataFrame(gradient_details).sort_values(by='gradient_range').reset_index(drop=True)
+    gradient_details_df = pd.DataFrame(gradient_details).sort_values(
+        by='gradient_range').reset_index(drop=True)
     return gradient_details_df
+
+
 def SingleSlopeGroups(df):
     df_dumpmin = df.dropna(subset=['min'])
     df_dumpmax = df.dropna(subset=['max'])
@@ -122,17 +142,20 @@ def SingleSlopeGroups(df):
     indexes.sort()
 
     single_slopes = []
-    
+
     for i in range(len(indexes)-1):
         start_lat = df.iloc[indexes[i]]['lat']
         start_lon = df.iloc[indexes[i]]['lon']
         end_lat = df.iloc[indexes[i+1]]['lat']
         end_lon = df.iloc[indexes[i+1]]['lon']
-        distance_covered = df.iloc[indexes[i+1]]['cumDistance'] - df.iloc[indexes[i]]['cumDistance']
-        elevation_change = df.iloc[indexes[i+1]]['cumElevation'] - df.iloc[indexes[i]]['cumElevation']
+        distance_covered = df.iloc[indexes[i+1]
+                                   ]['cumDistance'] - df.iloc[indexes[i]]['cumDistance']
+        elevation_change = df.iloc[indexes[i+1]
+                                   ]['cumElevation'] - df.iloc[indexes[i]]['cumElevation']
         elevation = df.iloc[indexes[i]]['ele']
         time_since_start = df.iloc[indexes[i]]['cumTime']
-        time_elapsed = df.iloc[indexes[i+1]]['cumTime'] - df.iloc[indexes[i]]['cumTime']
+        time_elapsed = df.iloc[indexes[i+1]]['cumTime'] - \
+            df.iloc[indexes[i]]['cumTime']
 
         single_slopes.append({
             'start_lat': start_lat,
@@ -146,9 +169,12 @@ def SingleSlopeGroups(df):
             'time_elapsed': time_elapsed
         })
     ss_df = pd.DataFrame(single_slopes)
-    return ss_df # This function returns every single slope group's dataframe for own purpose.
+    # This function returns every single slope group's dataframe for own purpose.
+    return ss_df
+
+
 # Reading and parsing GPX file.
-gpx_file_path = 'C:\\Users\\OZGUN\\Documents\\GitHub\\iuc-mak-pro-i\\gpx_files\\Afternoon_Ride.gpx'
+gpx_file_path = 'C:\\Users\\OZGUN\\Documents\\GitHub\\iuc-mak-pro-i\\gpx_files\\Aksam_Turu.gpx'
 gpxFile = GPXFile(gpx_file_path)
 gpxFile.print_info()
 
@@ -156,7 +182,7 @@ gpxFile.print_info()
 gpxDF = gpxFile.get_gpx_dataframe()
 # gpxDF_10 = gpxDF[gpxDF.index % 10 == 0] # Getting 1 row from every 10 row. Make calculations more smooth
 
-#Calculating speed, distance etc.
+# Calculating speed, distance etc.
 gpxDF = DataFrameCalculations(gpxDF)
 gpxDF = DataFrameSmoothing(gpxDF)
 gpxDF = GroupSlopes(gpxDF)
@@ -198,7 +224,8 @@ fig_ElevationMinMaxPoints.show()
 
 for i in range(len(ssDF)-1):   # for each row:
     # plt.plot([list of Xs], [list of Ys])
-    plt.plot([ssDF.iloc[i,7],ssDF.iloc[i+1,7]],[ssDF.iloc[i,6],ssDF.iloc[i+1,6]])
+    plt.plot([ssDF.iloc[i, 7], ssDF.iloc[i+1, 7]],
+             [ssDF.iloc[i, 6], ssDF.iloc[i+1, 6]])
 plt.show()
 
 #---------------------------------------------------------------------------------------#
